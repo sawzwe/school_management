e9b42f67bb5695dca3e0ee35859164382d9e4d4d
Saw Zwe Wai Yan <saw@sales-mind.ai>
2024-11-12 01:20:38 +0700

    Initial commit

---
+++ src/app/parent/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ParentPage = () => {
+  return (
+    <div>parent</div>
+  )
+}
+
+export default ParentPage
---
+++ src/app/student/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const StudentPage = () => {
+  return (
+    <div>student</div>
+  )
+}
+
+export default StudentPage
